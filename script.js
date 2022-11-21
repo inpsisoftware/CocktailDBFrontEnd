856a0abc587c5e9f3f7538119eb4d929d3bbcb1d
Javier Gonzalez <inpsisoftware@gmail.com>
2022-11-21 13:13:45 +0000

    Volver a protocolo no seguro

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('buscador__button').addEventListener('click', buscarDato
 
 function buscarDatos() {
     let cocktail = document.getElementById('buscador__input').value;
-    fetch('https://www.cocktailDBclone.somee.com/api/cocktail/buscar/' + cocktail)
+    fetch('http://www.cocktaildbclone.somee.com/api/cocktail/buscar/' + cocktail)
     .then(response => response.json())
     .then(data => console.log(data));
 }
