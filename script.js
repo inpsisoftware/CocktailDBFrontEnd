452da3d3cb8f2da7943c82389299b7c15138f4c0
Javier Gonzalez <inpsisoftware@gmail.com>
2022-11-21 12:59:45 +0000

    Cambiar a protocolo seguro

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('buscador__button').addEventListener('click', buscarDato
 
 function buscarDatos() {
     let cocktail = document.getElementById('buscador__input').value;
-    fetch('http://www.cocktailDBclone.somee.com/api/cocktail/buscar/' + cocktail)
+    fetch('https://www.cocktailDBclone.somee.com/api/cocktail/buscar/' + cocktail)
     .then(response => response.json())
     .then(data => console.log(data));
 }
